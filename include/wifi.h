2c3a73ab7d8950cd01055af94e0ca7401f17e13e
Bachelor_Thesis <tobias00@zedat.fu-berlin.de>
2026-02-20 17:36:08 +0100

    Removed global variable from main and unnecessary preprocessor commands

--- include/wifi.h
+++ include/wifi.h
@@ -1,9 +1,6 @@
 #ifndef WIFI_H
 #define WIFI_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif
 
 typedef enum {
     WIFI_STATUS_OK = 0,
@@ -14,9 +11,7 @@ typedef enum {
 wifi_status_t wifi_init(void);
 wifi_status_t wifi_check_connection(void);
 void handle_wifi_status(wifi_status_t);
-#ifdef __cplusplus
-}
-#endif
+
 
 #endif
 
