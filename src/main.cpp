ebe225348dfbaf328134e58eadc826a70cec10b6
Bachelor_Thesis <tobias00@zedat.fu-berlin.de>
2026-02-20 17:15:28 +0100

    Cleaned main from test methods

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,6 @@
 #include "main.h"
 
 
-#define HALL_PIN 34
 
 
 
@@ -22,29 +21,16 @@ wifi_status_t wifi_status;
 
 void setup() {
     Serial.begin(SERIAL_BAUD);
-    pinMode(HALL_PIN, INPUT);
     wifi_status_t wifi_status = wifi_init();
+    handle_wifi_status(wifi_status);
     
 }
 
-/*******
- * Überprüfe in jedem Durchgang, ob die Verbindung steht
- * 
- * 
- * 
- */
+
 
 
 
 void loop() {
-  int hall_effect_sensor_state = digitalRead(HALL_PIN);
-
-  if (hall_effect_sensor_state == LOW) {
-    Serial.println("Magnet erkannt!");
-  } else {
-    Serial.println("Kein Magnet");
-  }
-
     wifi_status = wifi_check_connection();
     handle_wifi_status(wifi_status);
 
