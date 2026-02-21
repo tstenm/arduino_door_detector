d88a4fa5624e0848745ccd2f49e97f8eb2efaefb
Bachelor_Thesis <tobias00@zedat.fu-berlin.de>
2026-02-22 00:20:31 +0100

    Vollständige Implementierung des Hall-Effect Moduls bzw. des Door-State Moduls (Namensgebung steht nocht nicht endgültig fest). Alle Funktionen wurden getestet.

--- src/hall_sensor/hall_sensor.cpp
+++ src/hall_sensor/hall_sensor.cpp
@@ -12,22 +12,69 @@
  *
  * @return  void
  ******************************************************************************/
-
+//tested
 void init_hall_pin(){
     pinMode(HALL_PIN, INPUT);
 }
-
-
-bool tmp_hall_sensor()
-{
-  int hall_effect_sensor_state = digitalRead(HALL_PIN);
-  if (hall_effect_sensor_state == LOW) {
-
-    Serial.println("Magnet");
-    return true;
-  } else {
-    Serial.println("njiet Magnet");
-    return false;
-  } 
+/*******************************************************************************
+ * @brief   Reads out hall effect pin
+ *
+ * @param   /
+ *
+ * @return  DOOR_OPEN or DOOR_CLOSED
+ ******************************************************************************/
+//tested
+hall_state_t read_hall_sensor(){
+    if(digitalRead(HALL_PIN)){
+        return DOOR_CLOSED;
+    }
+    else{
+        return DOOR_OPEN;
+    }
 }
 
+
+
+
+
+/*******************************************************************************
+ * @brief   Inits the door_state_t struct with the first value read from the hall effect sensor
+ *
+ * @param   hall_state_t, door_state_t
+ *
+ * @return  void
+ ******************************************************************************/
+// tested
+void init_door_state(door_state_t * const door_state,
+                     const hall_state_t hall_state)
+{
+    door_state->current_value  = hall_state;
+    door_state->previous_value = hall_state;
+}
+
+/*******************************************************************************
+ * @brief   Updates the door_state struct after a new value has been read
+ *
+ * @param   hall_state_t, door_state_t
+ *
+ * @return  void
+ ******************************************************************************/
+//tested
+void update_door_state(door_state_t * const door_state, const hall_state_t hall_state){
+    door_state->previous_value  = door_state->current_value;
+    door_state->current_value = hall_state;
+}
+
+
+
+/*******************************************************************************
+ * @brief   Compares both values in door_state_t, to check for updates. 
+ *
+ * @param   door_state_t
+ *
+ * @return  true or false
+ ******************************************************************************/
+//tested 
+bool door_state_altered(door_state_t * const door_state){
+    return(door_state->current_value != door_state->previous_value);
+}
