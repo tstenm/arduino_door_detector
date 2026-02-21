7ca03994d7e4ed57233e3f1ae8241ff91a6f0eb7
Bachelor_Thesis <tobias00@zedat.fu-berlin.de>
2026-02-21 18:22:17 +0100

    Speicherung des rohen Zwischenstandes des Hall-Effect-Moduls. Bisher init des Pins und eine Debug Funktion, sowie eine typedef

---
+++ include/hall_sensor.h
@@ -0,0 +1,10 @@
+#include <stdbool.h>
+
+
+#define HALL_PIN 34
+
+
+void init_hall_pin(void);
+
+
+bool tmp_hall_sensor(void);
--- src/main.cpp
+++ src/main.cpp
@@ -2,36 +2,24 @@
 #include <Arduino.h>
 #include "wifi.h"
 #include "main.h"
+#include "hall_sensor.h"
 
 
 
 
 
-
-
-/******************************************
- *
- *
- *
- *
- *
- *
- * ********/
 
 void setup() {
     Serial.begin(SERIAL_BAUD);
     wifi_status_t wifi_status = wifi_init();
     handle_wifi_status(wifi_status);
-    
+    init_hall_pin();
 }
-
-
 
 
 
 void loop() {
     wifi_status_t wifi_status = wifi_check_connection();
     handle_wifi_status(wifi_status);
-
-
+    tmp_hall_sensor();
 }
